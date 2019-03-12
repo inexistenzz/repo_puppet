c4fc64fdc238fbe5ef80d522a630ed48ddd21da2
inexistenzz <48113580+inexistenzz@users.noreply.github.com>
2019-03-12 08:59:32 +0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
-  include dockeragent
-  dockeragent::node {'web.puppet.vm':}
-  dockeragent::node {'db.puppet.vm':}
+  include vagrant
+  vagrant::node {'web.puppet.vm':}
+  vagrant::node {'db.puppet.vm':}
  }

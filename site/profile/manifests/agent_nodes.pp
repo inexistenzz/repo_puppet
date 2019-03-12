73c57252c1bff16dbc12954fa095660a5768c804
inexistenzz <48113580+inexistenzz@users.noreply.github.com>
2019-03-12 09:21:10 +0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include vagrant
-  vagrant::node {'web.puppet.vm':}
-  vagrant::node {'db.puppet.vm':}
+  vagrant::box {'web.puppet.vm':}
+  vagrant::box {'db.puppet.vm':}
  }
